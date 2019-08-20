b548ee8da02e73fdf836de255abf4ddd02e3d897
shelbaydo <38807661+shelbaydo@users.noreply.github.com>
2019-08-20 09:20:33 +0800

    添加首页

---
+++ src/main/java/com/ncu/emailManagement/controller/BaseController.java
@@ -0,0 +1,54 @@
+package com.ncu.emailManagement.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * app-store-com.ncu.qppstore.common
+ * created by LI LICHUNYAN at 2019/8/7
+ */
+@Controller
+public class BaseController {
+    /**
+     * 获取request
+     * @return
+     */
+    public static HttpServletRequest getRequest() {
+        ServletRequestAttributes attrs =(ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        return attrs.getRequest();
+    }
+
+    /**
+     * 获取response
+     * @return
+     */
+    public static HttpServletResponse getResponse() {
+        HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
+        return response;
+    }
+
+    /**
+     * 获取session
+     * @return
+     */
+    public static HttpSession getSession() {
+        HttpSession session = null;
+        try {
+            session = getRequest().getSession();
+        } catch (Exception e) {}
+        return session;
+    }
+    @ExceptionHandler(Exception.class)
+    public String exceptionHandler(Exception exception, Model model){
+        model.addAttribute("exception",exception);
+        return "error";
+    }
+}
